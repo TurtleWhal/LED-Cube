c6126523dd72bcdecfe5a25eee9a26162f64d7e5
Garrett Jordan <garrettrjordan@gmail.com>
2024-12-16 20:58:33 -0800

    rgb

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,55 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, PIN,
                                                    NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
                                                NEO_GRB + NEO_KHZ800);
 
+// Function to convert HSL to RGB
+uint16_t HSL(float h, float s, float l)
+{
+  float c = (1.0 - abs(2.0 * l - 1.0)) * s;             // Chroma
+  float x = c * (1.0 - abs(fmod(h / 60.0, 2.0) - 1.0)); // Intermediate value
+  float m = l - c / 2.0;
+
+  float r1, g1, b1;
+
+  if (h >= 0 && h < 60)
+  {
+    r1 = c;
+    g1 = x;
+    b1 = 0;
+  }
+  else if (h >= 60 && h < 120)
+  {
+    r1 = x;
+    g1 = c;
+    b1 = 0;
+  }
+  else if (h >= 120 && h < 180)
+  {
+    r1 = 0;
+    g1 = c;
+    b1 = x;
+  }
+  else if (h >= 180 && h < 240)
+  {
+    r1 = 0;
+    g1 = x;
+    b1 = c;
+  }
+  else if (h >= 240 && h < 300)
+  {
+    r1 = x;
+    g1 = 0;
+    b1 = c;
+  }
+  else
+  {
+    r1 = c;
+    g1 = 0;
+    b1 = x;
+  }
+
+  return matrix.Color((r1 + m) * 255, (g1 + m) * 255, (b1 + m) * 255);
+}
+
 void setup()
 {
   matrix.begin();
@@ -28,7 +77,7 @@ void setup()
   // matrix.show();
 }
 
-// uint16_t i = 0;
+int c = 0;
 
 void loop()
 {
@@ -63,11 +112,33 @@ void loop()
   //   delay(100);
   // }
 
+  // for (int i = 0; i < 8; i++)
+  // {
+  //   matrix.clear();
+  //   matrix.drawCircle(0, 0, i, matrix.Color(255, 128, 0));
+  //   matrix.drawCircle(7, 7, i, matrix.Color(128, 128, 0));
+  //   matrix.drawCircle(0, 7, i, matrix.Color(0, 128, 255));
+  //   matrix.drawCircle(7, 0, i, matrix.Color(255, 0, 255));
+  //   matrix.show();
+  //   delay(100);
+  // }
+
   for (int i = 0; i < 8; i++)
   {
-    matrix.clear();
-    matrix.drawCircle(0, 0, i, matrix.Color(255, 128, 0));
-    matrix.show();
-    delay(100);
+    for (int j = 0; j < 8; j++)
+    {
+      // matrix.clear();
+      matrix.drawPixel(j, i, HSL(((i + j + c) * (360 / (7 + 7))) % 360, 1, 0.5)); // rgb
+      // matrix.drawPixel(j, i, matrix.ColorHSV(((i * j) * 4) * (65535 / 255))); // rgb
+      // matrix.drawPixel(j, i, matrix.ColorHSV((65535 / 64) * (i*8 + j), 255, 255)); //
+      // matrix.drawPixel(j, i, matrix.Color(random(0, 255), random(0, 255), random(0, 255))); // random
+    }
   }
+
+  matrix.show();
+  delay(50);
+
+  c = c - 1 % 360;
+  if (c < 0)
+    c = 360;
 }
